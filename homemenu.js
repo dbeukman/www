9bb8babe06c9b0fd352ac8119b2ff339e83bb3b3
Dirk <dbeukman.delia@beukman.co.za>
2020-05-12 10:47:05 +0200

    homemenu.js

--- homemenu.js
+++ homemenu.js
@@ -1,4 +1,4 @@
 document.write("<a class='active' href='index.html'>Main Menu</a>");
 document.write("<a href='admin.html'>Admin</a>");
 document.write("<a href='workingplace.html'>Working Places</a>");
-document.write("<a href='production'>Production</a><a href='reports' class='increasefont'>Reports</a> <!-- Ek wil hierdie font groter maak wanneer jy met jou muis daaroor gaan??? -->");
+document.write("<a href='production'>Production</a><a href='reports'>Reports</a>");
